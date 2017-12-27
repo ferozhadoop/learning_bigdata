ef47702ddf41a8e136619e4451f9e3a12aeaa6b0
ferozhadoop <34747917+ferozhadoop@users.noreply.github.com>
2017-12-27 22:46:48 +0530

    Add files via upload

---
+++ python/learning_python/classassingment.py
@@ -0,0 +1,46 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+class Arithmetic_operations:       
+    def addition(self,a,b):
+        print('additon method',a+b)
+    def sub(self,a,b):
+        print('sub method',a-b)
+    def mult(self,a,b):
+        print('mlt method',a*b)
+    def div(self,a,b):
+        print('div method',a%b)
+
+
+# In[2]:
+
+
+o1 =Arithmetic_operations()
+
+
+# In[3]:
+
+
+o1.mult(50,60)
+
+
+# In[4]:
+
+
+o1.div(50,60)
+
+
+# In[5]:
+
+
+o1.addition(50,60)
+
+
+# In[6]:
+
+
+o1.sub(50,60)
+
