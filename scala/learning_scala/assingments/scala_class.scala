88e988aa06729230f6a2edc1a0b06bd3799e9f2c
ferozhadoop <34747917+ferozhadoop@users.noreply.github.com>
2017-12-27 22:21:32 +0530

    Add files via upload

---
+++ scala/learning_scala/assingments/scala_class.scala
@@ -0,0 +1,58 @@
+// Databricks notebook source
+val a =10
+
+// COMMAND ----------
+
+val b =20.5
+
+// COMMAND ----------
+
+val c = "hello"
+
+// COMMAND ----------
+
+val d= true 
+
+// COMMAND ----------
+
+val e: Int =10
+
+// COMMAND ----------
+
+val f: Double =10.5
+
+// COMMAND ----------
+
+val g: String ="hello"
+
+// COMMAND ----------
+
+val h:Boolean = true 
+
+// COMMAND ----------
+
+val i: String = 10
+
+// COMMAND ----------
+
+val k=10
+
+// COMMAND ----------
+
+val k=20
+
+// COMMAND ----------
+
+var z=30
+
+// COMMAND ----------
+
+z=50
+
+// COMMAND ----------
+
+k=40
+
+// COMMAND ----------
+
+
